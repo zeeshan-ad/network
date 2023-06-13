293e698a257c24c5e716314db7149f60c9144b48
zeeshan-ad <zeeshanahmed959@gmail.com>
2023-06-13 12:04:54 +0530

    landing page

--- client/screens/Landing.jsx
+++ client/screens/Landing.jsx
@@ -17,7 +17,6 @@ const Landing = ({ navigation }) => {
   const [PhoneNumber, setPhoneNumber] = useState(null);
   const [formattedValue, setFormattedValue] = useState(null);
   const phoneInput = useRef(null);
-  const [LoginForm, setLoginForm] = useState(false)
 
   const [fontsLoaded] = useFonts({
     'Pacifico': require('../assets/fonts/Pacifico-Regular.ttf'),
@@ -44,60 +43,56 @@ const Landing = ({ navigation }) => {
       <StatusBar style="light" />
       <DismissKeyboard>
         <View style={styles.container}>
-          {LoginForm ? (
-            <View style={{ height: '80%', justifyContent: 'center' }}>
-              <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginBottom: 10 }}>
-                <Text style={styles.logo}>{APP_NAME}</Text>
-              </View>
-              <View style={{ flexDirection: 'row', justifyContent: 'center', gap: 10, alignItems: 'center', }}>
-                <View style={{ borderWidth: 1, borderColor: theme.colors.light }}>
-                  <PhoneInput
-                    ref={phoneInput}
-                    defaultCode="IN"
-                    defaultValue={PhoneNumber}
-                    disableArrowIcon={true}
-                    placeholder=" "
-                    flagButtonStyle={{
-                      paddingLeft: 10, backgroundColor: theme.colors.primary, width: 50, height: 50, borderColor: theme.colors.primary
-                    }}
-                    textContainerStyle={{
-                      backgroundColor: theme.colors.primary, color: theme.colors.light, fontSize: fontSizes.large, height: 50, borderColor: theme.colors.primary,
-                      justifyContent: 'center', alignItems: 'center'
-                    }}
-                    textInputStyle={{ color: theme.colors.light, fontSize: fontSizes.large }}
-                    codeTextStyle={{ color: theme.colors.light, fontSize: fontSizes.large, height: 50, paddingTop: 15 }}
-                    layout="first"
-                    containerStyle={{ color: theme.colors.light, backgroundColor: theme.colors.primary, alignItems: 'center' }}
-                    onChangeText={(text) => {
-                      setPhoneNumber(text);
-                    }}
-                    onChangeFormattedText={(text) => {
-                      setFormattedValue(text);
-                    }}
-                    withDarkTheme
-                    withShadow
-                  />
-                </View>
-                <Pressable
-                  onPress={() => {
-                    if (PhoneNumber?.length > 9) {
-                      navigation.navigate('OTP', { phone: PhoneNumber, code: phoneInput.current.state.code, formattedValue: formattedValue })
-                    }
-                  }}
-                  style={[styles.button, { backgroundColor: PhoneNumber?.length > 9 ? theme.colors.secondary : theme.colors.grey }]}>
-                  <MaterialCommunityIcons name="arrow-top-right" size={30} color="black" />
-                </Pressable>
-              </View>
-              <View>
-              </View>
+          <View style={{ height: '80%', justifyContent: 'center' }}>
+            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginBottom: 10 }}>
+              <Text style={styles.logo}>{APP_NAME}</Text>
             </View>
-          ) : (
-            <LandingBackground setLoginForm={setLoginForm}/>
-          )}
+            <View style={{ flexDirection: 'row', justifyContent: 'center', gap: 10, alignItems: 'center', }}>
+              <View style={{ borderWidth: 1, borderColor: theme.colors.light }}>
+                <PhoneInput
+                  ref={phoneInput}
+                  defaultCode="IN"
+                  defaultValue={PhoneNumber}
+                  disableArrowIcon={true}
+                  placeholder=" "
+                  flagButtonStyle={{
+                    paddingLeft: 10, backgroundColor: theme.colors.primary, width: 50, height: 50, borderColor: theme.colors.primary
+                  }}
+                  textContainerStyle={{
+                    backgroundColor: theme.colors.primary, color: theme.colors.light, fontSize: fontSizes.large, height: 50, borderColor: theme.colors.primary,
+                    justifyContent: 'center', alignItems: 'center'
+                  }}
+                  textInputStyle={{ color: theme.colors.light, fontSize: fontSizes.large }}
+                  codeTextStyle={{ color: theme.colors.light, fontSize: fontSizes.large, height: 50, paddingTop: 15 }}
+                  layout="first"
+                  containerStyle={{ color: theme.colors.light, backgroundColor: theme.colors.primary, alignItems: 'center' }}
+                  onChangeText={(text) => {
+                    setPhoneNumber(text);
+                  }}
+                  onChangeFormattedText={(text) => {
+                    setFormattedValue(text);
+                  }}
+                  withDarkTheme
+                  withShadow
+                />
+              </View>
+              <Pressable
+                onPress={() => {
+                  if (PhoneNumber?.length > 9) {
+                    navigation.navigate('OTP', { phone: PhoneNumber, code: phoneInput.current.state.code, formattedValue: formattedValue })
+                  }
+                }}
+                style={[styles.button, { backgroundColor: PhoneNumber?.length > 9 ? theme.colors.secondary : theme.colors.grey }]}>
+                <MaterialCommunityIcons name="arrow-top-right" size={30} color="black" />
+              </Pressable>
+            </View>
+            <View>
+            </View>
+          </View>
         </View>
       </DismissKeyboard>
       <Text style={{ position: 'absolute', bottom: 30, right: 0, left: 0, textAlign: 'center', color: theme.colors.light }}>
-        {LoginForm && 'Standard charges apply.'}  By continuing, you agree{'\n'}to our  <Link style={{ color: theme.colors.secondary }} to={{ screen: '' }}>Privacy Policy</Link> and
+        Standard charges apply By continuing, you agree{'\n'}to our  <Link style={{ color: theme.colors.secondary }} to={{ screen: '' }}>Privacy Policy</Link> and
         <Link style={{ color: theme.colors.secondary }} to={{ screen: '' }}> Terms of Service</Link>.
       </Text>
     </KeyboardAvoidingView>
